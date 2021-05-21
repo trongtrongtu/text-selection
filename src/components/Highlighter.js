44737a7ed24c971938d0ee958f7badc8bf425cdf
trongtrongtu <trongtrongtu169@gmail.com>
2021-05-21 21:20:08 +0700

    Modify selection

--- src/components/Highlighter.js
+++ src/components/Highlighter.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-import { Modal } from 'antd';
-import SelectionHighlighter from "react-highlight-selection";
-import TextSelector from 'text-selection-react';
-import { InteractiveHighlighter } from 'react-interactive-highlighter';
 import $ from 'jquery';
 import "./styles.css";
 import 'antd/dist/antd.css';
@@ -12,23 +8,71 @@ class Highlighter extends Component {
     isModalVisible: false,
     textHighlight: '',
     dataArr: [],
-    description: 'Let there be light, let there be Sun.',
+    descriptiondefault: 'It wasn’t just the rules that had changed. The statistical revolution that swept into Major League Baseball shortly after the turn of the twenty-first century had arrived, with a few years’ delay, in the N.B.A., bringing a greater emphasis on three-point shooting from the corners and on finding openings near the basket, for high-percentage attempts. As a result, teams were reshuffling their depth charts in favor of mobility over size. “Small ball,” Bryant (and others) called it. The kind of versatile player, like Bryant, who could shoot well from anywhere on the court was no longer so highly prized, because twenty-foot jumpers were a low-percentage gamble, by definition. “I’ve always been more interested in the creative side of the game, like how things happen, why things happen, as opposed to just the numbers,” Bryant told me. “Numbers have never felt fun to me.”',
+    description: 'It wasn’t just the rules that had changed. The statistical revolution that swept into Major League Baseball shortly after the turn of the twenty-first century had arrived, with a few years’ delay, in the N.B.A., bringing a greater emphasis on three-point shooting from the corners and on finding openings near the basket, for high-percentage attempts. As a result, teams were reshuffling their depth charts in favor of mobility over size. “Small ball,” Bryant (and others) called it. The kind of versatile player, like Bryant, who could shoot well from anywhere on the court was no longer so highly prized, because twenty-foot jumpers were a low-percentage gamble, by definition. “I’ve always been more interested in the creative side of the game, like how things happen, why things happen, as opposed to just the numbers,” Bryant told me. “Numbers have never felt fun to me.”',
     text: "I saw the best minds of my generation destroyed by madness, starving hysterical naked, dragging themselves...",
     highlights: [],
     position1: null,
     position2: null,
-    textSelect: ''
+    textSelect: '',
   }
 
   componentDidMount() {
-    const { position1, position2, textSelect } = this.state
     let _this = this;
+    $("#longtext").click(function (e) {
+      var that = this;
+      setTimeout(function () {
+        var dblclick = parseInt($(that).data('double'), 10);
+        if (dblclick > 0) {
+          $(that).data('double', dblclick - 1);
+        } else {
+          var selection = window.getSelection().toString();
+          if (selection && selection !== ' ') {
+            $('#selTxt').val('');
+            var x = e.pageX;
+            var y = e.pageY;
+            placeTooltip(x, y);
+            $("#tooltip").show();
+            var range = window.getSelection().getRangeAt(0);
+            _this.setState({
+              position1: window.getSelection().anchorOffset,
+              position2: window.getSelection().extentOffset,
+              textSelect: selection.toString(),
+              range,
+            })
+            $(document).on('click', function (e) {
+              if ($(e.target).closest("#tooltip").length === 0) {
+                $("#tooltip").hide();
+              }
+            });
+          }
+          // });
+        }
+      }, 300);
+    }).dblclick(function (e) {
+      $(this).data('double', 2);
+      var selection = window.getSelection().toString();
+      if (selection && selection !== ' ') {
+        $('#selTxt').val('');
+        var x = e.pageX;
+        var y = e.pageY;
+        placePopup(x, y);
+        $("#popup").show();
+        var range = window.getSelection().getRangeAt(0);
+        _this.setState({
+          position1: window.getSelection().anchorOffset,
+          position2: window.getSelection().extentOffset,
+          textSelect: selection.toString(),
+          range,
+        })
+        $(document).on('click', function (e) {
+          if ($(e.target).closest("#popup").length === 0) {
+            $("#popup").hide();
+          }
+        });
+      }
+    });
     $("#addBtn").click(function () {
-      console.log(position1, position2, textSelect)
-      // $("#names")
-      //   .append($('<li>').append(
-      //     $('#selTxt').val()
-      //   ));
       $("#tooltip").hide();
     });
     function placeTooltip(x_pos, y_pos) {
@@ -38,124 +82,63 @@ class Highlighter extends Component {
         position: 'absolute'
       });
     }
-    $('#longtext').mouseup(function (e) {
-      // get selected text
-      // http://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
-      var selection = window.getSelection().toString();
-      if (selection) {
-        $('#selTxt').val(selection.toString());
-        var x = e.pageX;
-        var y = e.pageY;
-        placeTooltip(x, y);
-        $("#tooltip").show();
-        console.log('selection: ', window.getSelection().anchorOffset, window.getSelection().extentOffset)
-        _this.setState({
-          position1: window.getSelection().anchorOffset,
-          position2: window.getSelection().extentOffset,
-          textSelect: selection.toString()
-        })
-      }
-    });
-  }
-
-  selectionHandler = (selection) => {
-    //do something with selection
-    console.log(selection);
-    if (selection?.selection) {
-      this.showModal(selection?.selection);
+    function placePopup(x_pos, y_pos) {
+      $("#popup").css({
+        top: y_pos + 'px',
+        left: x_pos + 'px',
+        position: 'absolute'
+      });
     }
   }
 
-  showModal = (value) => {
-    const { dataArr } = this.state
-    this.setState({
-      isModalVisible: true,
-      textHighlight: value,
-      // dataArr: [...dataArr, value]
-    })
-  };
-
-  handleOk = () => {
-    this.setState({
-      isModalVisible: false
-    })
-  };
-
-  handleCancel = () => {
-    this.setState({
-      isModalVisible: false
-    })
-  };
-
-  handler = (html, text) => {
-    console.log('text: ', text)
-    console.log('html: ', html)
-  };
-
-  selectionHandler = (selected, startIndex, numChars) => {
-    this.showModal(selected)
-    this.setState({
-      text: this.state.text,
-      highights: this.state.highlights.push({
-        startIndex: startIndex,
-        numChars: numChars,
-        note: 'aaaaa'
-      })
-    })
+  onchange = (event) => {
+    event.preventDefault();
+    const { range } = this.state
+    var selectionContents = range.extractContents();
+    var div = document.createElement("span");
+    // console.log('div.style: ', div.style.background)
+    div.style.background = "red";
+    div.style.cursor = "pointer";
+    div.title = event.target.textSelect.value
+    div.appendChild(selectionContents);
+    range.insertNode(div);
   }
 
-  onchange = (value) => {
-    const { position1, position2, textSelect, description } = this.state
-    const data1 = `<span class="tooltip" style="background: red;">${textSelect}<span class="tooltiptext">Tooltip text</span></span>`
-    const data2 = description.slice(0,position1)
-    const data3 = description.slice(position2,description.length)
-    console.log('data3: ', value.target)
-    this.setState({
-      description: data2.concat(data1,data3)
-    })
+  compare = (a, b) => {
+    if (a.start > b.start) {
+      return -1;
+    }
+    if (a.start < b.start) {
+      return 1;
+    }
+    return 0;
   }
 
   render() {
 
-    const { isModalVisible, textHighlight, dataArr, description, highlights,position1, position2, textSelect } = this.state
-    const { text } = this.props
-    console.log(position1, position2, textSelect)
+    const { description, textSelect } = this.state
     return (
       <>
-        {/* <SelectionHighlighter
-          text={text}
-          selectionHandler={this.selectionHandler}
-          customClass="custom-class"
-        />
-        <div dangerouslySetInnerHTML={{ __html: description }} /> */}
-        {/* <TextSelector
-        events={[
-        {
-            text: 'Submit',
-            handler: this.handler
-        }
-        ]}
-        color={'yellow'}
-        colorText={true}
-    /> */}
-        {/* <InteractiveHighlighter
-          text={this.state.text}
-          highlights={this.state.highlights}
-          customClass='highlighted'
-          selectionHandler={this.selectionHandler}
-        />
-        <h1 style={{ marginTop: '300px' }}>Here In events you can add as many event as you want. like currently i am using only one event then it will be shown as</h1> */}
         <ul id="names">
-
         </ul>
-        <div id="tooltip">Selected text:
-  <input id='selTxt' type="text"></input>
-          <button type="button" id='addBtn' name='a' onClick={this.onchange}>Add</button>
+        <div id="tooltip">
+          <div style={{ position: 'relative' }}>
+            <span>{textSelect}</span>
+            <span style={{ position: 'absolute', top: 0, right: 5, cursor: 'pointer' }} onClick={() => document.getElementById('tooltip').style.display = "none"}>x</span>
+          </div>
+          <form onSubmit={this.onchange}>
+            <input id='selTxt' type="text" name="textSelect"></input>
+            <button type="submit" id='addBtn'>Add</button>
+          </form>
+        </div>
+        <div id="popup" style={{ width: '30%' }}>
+          <div style={{ position: 'relative' }}>
+            <span>{textSelect}</span>
+            <span style={{ position: 'absolute', top: 0, right: 5, cursor: 'pointer' }} onClick={() => document.getElementById('popup').style.display = "none"}>x</span>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: `<figure class="table"><table style="background-color:rgb(255, 255, 255);"><tbody><tr><td style="padding:0px;width:18px;"><strong>◎</strong></td><td style="padding:0px;width:2000px;" colspan="3">[,revə'lu:∫n]</td></tr><tr><td style="padding:0px;width:18px;"><strong>※</strong></td><td style="padding:0px;width:2000px;" colspan="3"><strong>danh từ</strong></td></tr><tr><td style="padding:0px;">&nbsp;</td><td style="padding:0px;width:18px;"><strong>■</strong></td><td style="padding:0px;width:2000px;" colspan="2">sự xoay vòng; vòng quay; vòng, tua</td></tr><tr><td style="padding:0px;" colspan="2">&nbsp;</td><td style="padding:0px;width:18px;"><strong>☆</strong></td><td style="padding:0px;width:2000px;">revolutions per minute</td></tr><tr><td style="padding:0px;" colspan="3">&nbsp;</td><td style="padding:0px;width:2000px;">số vòng quay mỗi phút</td></tr><tr><td style="padding:0px;">&nbsp;</td><td style="padding:0px;width:18px;"><strong>■</strong></td><td style="padding:0px;width:2000px;" colspan="2">(toán học); (thiên văn học) sự xoay vòng</td></tr><tr><td style="padding:0px;">&nbsp;</td><td style="padding:0px;width:18px;"><strong>■</strong></td><td style="padding:0px;width:2000px;" colspan="2">cuộc cách mạng (nhất là bằng vũ lực, lật đổ một chế độ cai trị)</td></tr><tr><td style="padding:0px;" colspan="2">&nbsp;</td><td style="padding:0px;width:18px;"><strong>☆</strong></td><td style="padding:0px;width:2000px;">the socialist revolution</td></tr><tr><td style="padding:0px;" colspan="3">&nbsp;</td><td style="padding:0px;width:2000px;">cuộc cách mạng xã hội chủ nghĩa</td></tr><tr><td style="padding:0px;" colspan="2">&nbsp;</td><td style="padding:0px;width:18px;"><strong>☆</strong></td><td style="padding:0px;width:2000px;">the national democratic revolution</td></tr><tr><td style="padding:0px;" colspan="3">&nbsp;</td><td style="padding:0px;width:2000px;">cuộc cách mạng dân tộc dân chủ</td></tr><tr><td style="padding:0px;">&nbsp;</td><td style="padding:0px;width:18px;"><strong>■</strong></td><td style="padding:0px;width:2000px;" colspan="2">cuộc cách mạng (sự thay đổi hoàn toàn về phương pháp, hoàn cảnh..)</td></tr><tr><td style="padding:0px;" colspan="2">&nbsp;</td><td style="padding:0px;width:18px;"><strong>☆</strong></td><td style="padding:0px;width:2000px;">a revolution in the treatment of cancer</td></tr><tr><td style="padding:0px;" colspan="3">&nbsp;</td><td style="padding:0px;width:2000px;">một cuộc cách mạng trong cách điều trị ung thư</td></tr><tr><td style="padding:0px;" colspan="2">&nbsp;</td><td style="padding:0px;width:18px;"><strong>☆</strong></td><td style="padding:0px;width:2000px;">a technological revolution</td></tr><tr><td style="padding:0px;" colspan="3">&nbsp;</td><td style="padding:0px;width:2000px;">một cuộc cách mạng trong công nghệ</td></tr></tbody></table></figure>` }}></div>
         </div>
         <div id='longtext' dangerouslySetInnerHTML={{ __html: description }}></div>
-        <Modal visible={isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
-          <div>{textHighlight}</div>
-        </Modal>
       </>
     );
   }
