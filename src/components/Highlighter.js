ce1ef5a22e8434f4008ae8b578d144e7b42170b0
trongtrongtu <trongtrongtu169@gmail.com>
2021-05-27 14:17:33 +0700

    Modify text selection

--- src/components/Highlighter.js
+++ src/components/Highlighter.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import $ from 'jquery';
 import "./styles.css";
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 
 let that = null
 let _this = null
@@ -9,6 +9,8 @@ let inputValue = null
 let checkTooltip = false
 let idTooltip = null
 let classTooltip = null
+let div = null
+let checkColor = false
 
 class Highlighter extends Component {
   state = {
@@ -16,7 +18,8 @@ class Highlighter extends Component {
     textSelect: '',
     range: null,
     key: Math.random(),
-    textTranslate: ''
+    textTranslate: '',
+    checkPopup: false,
   }
 
   componentDidMount() {
@@ -43,24 +46,56 @@ class Highlighter extends Component {
             _this.setState({
               textSelect: selection.toString(),
               range,
+              checkPopup: true
             })
             $('#selTxt').on('input', function (e) {
               inputValue = e.currentTarget.value
+            });
+            let selectionContents = _this.state.range.extractContents();
+            div = document.createElement("span");
+            div.id = `name_tooltip_${Math.random()}`
+            div.className = 'custom-class'
+            div.title = inputValue
+            div.appendChild(selectionContents);
+            _this.state.range.insertNode(div);
+            _this.setState({
+              key: Math.random()
+            })
+            $("#addBtn").click(function () {
+              checkTooltip = true
+              // if (classTooltip === 'custom-class') {
+              checkColor = true
+              $(div).replaceWith(`<span id=name_tooltip_${Math.random()} class="custom-class custom-class-replace" title=${inputValue}>${$(div).text()}</span>`);
+              _this.setState({
+                key: Math.random()
+              })
+              // } else {
+              //   $(div).replaceWith(`<span id=name_tooltip_${Math.random()} class="custom-class" title=${inputValue}>${$(div).text()}</span>`);
+              // }
+            });
+            $("#removeBtn").click(function () {
+              $("#tooltip").hide();
+              checkColor = false
+              $(div).replaceWith($(div).text());
             });
             $(document).on('click', function (e) {
               if ($(e.target).closest("#tooltip").length === 0) {
                 $("#tooltip").hide();
                 if (inputValue && checkTooltip) {
                   checkTooltip = false
-                  let selectionContents = _this.state.range.extractContents();
-                  let div = document.createElement("span");
-                  div.id = `name_tooltip_${Math.random()}`
-                  div.className = 'custom-class'
-                  div.title = inputValue
-                  div.appendChild(selectionContents);
-                  _this.state.range.insertNode(div);
+                  $(div).replaceWith(`<span id=name_tooltip_${Math.random()} class="${!checkColor ? 'custom-class' : 'custom-class custom-class-replace'}" title=${inputValue}>${$(div).text()}</span>`);
+                  checkColor = false
                   _this.setState({
-                    key: Math.random()
+                    key: Math.random(),
+                    checkPopup: false
+                  })
+                } else if (!inputValue && checkTooltip) {
+                  checkTooltip = false
+                  checkColor = false
+                  $(div).replaceWith($(div).text());
+                  _this.setState({
+                    key: Math.random(),
+                    checkPopup: false
                   })
                 }
               }
@@ -97,8 +132,8 @@ class Highlighter extends Component {
     });
     function placeTooltip(x_pos, y_pos) {
       $("#tooltip").css({
-        top: y_pos - 75 + 'px',
-        left: x_pos - 150 + 'px',
+        top: y_pos - 73 + 'px',
+        left: x_pos - 170 + 'px',
         position: 'absolute'
       });
     }
@@ -112,53 +147,64 @@ class Highlighter extends Component {
   }
 
   componentDidUpdate() {
+    const { checkPopup } = this.state
     that = this
-    $(".custom-class").hover(
-      function (e) {
-        inputValue = e.currentTarget.title
-        idTooltip = `#${e.currentTarget.id}`
-        classTooltip = $(e.target).attr('class')
-        _this = this;
-        let e_hover = e
-        $('#selTxtHover').val(e.currentTarget.title);
-        let x = e.pageX;
-        let y = e.pageY;
-        placeTooltip(x, y);
-        $("#tooltipHover").show();
-        $('#selTxtHover').on('input', function (e) {
-          checkTooltip = true
-          inputValue = e.currentTarget.value
-        });
-        $("#addBtnHover").click(function () {
-          checkTooltip = true
-          if (classTooltip === 'custom-class') {
-            $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class custom-class-replace" title=${inputValue}>${$(_this).text()}</span>`);
-          } else {
-            $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class" title=${inputValue}>${$(_this).text()}</span>`);
-          }
-        });
-        $(document).on('click', function (e) {
-          if ($(e.target).closest("#tooltipHover").length === 0) {
-            $("#tooltipHover").hide();
-            if (inputValue && checkTooltip && inputValue !== e_hover.currentTarget.title) {
-              checkTooltip = false
+    if (!checkPopup) {
+      $(".custom-class").hover(
+        function (e) {
+          inputValue = e.currentTarget.title
+          idTooltip = `#${e.currentTarget.id}`
+          classTooltip = $(e.target).attr('class')
+          _this = this;
+          let e_hover = e
+          $('#selTxtHover').val(e.currentTarget.title);
+          let x = e.pageX;
+          let y = e.pageY;
+          placeTooltip(x, y);
+          $("#tooltipHover").show();
+          $('#selTxtHover').on('input', function (e) {
+            checkTooltip = true
+            inputValue = e.currentTarget.value
+          });
+          $("#addBtnHover").click(function () {
+            checkTooltip = true
+            if (classTooltip === 'custom-class') {
+              $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class custom-class-replace" title=${inputValue}>${$(_this).text()}</span>`);
+            } else {
               $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class" title=${inputValue}>${$(_this).text()}</span>`);
-            } else if (!inputValue && checkTooltip) {
-              checkTooltip = false
-              $(_this).replaceWith($(_this).text());
             }
-          }
+          });
+          $("#removeBtnHover").click(function () {
+            $("#tooltipHover").hide();
+            $(_this).replaceWith($(_this).text());
+          });
+          $(document).on('click', function (e) {
+            if ($(e.target).closest("#tooltipHover").length === 0) {
+              $("#tooltipHover").hide();
+              if (inputValue && checkTooltip && inputValue !== e_hover.currentTarget.title) {
+                checkTooltip = false
+                if (classTooltip === 'custom-class') {
+                  $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class" title=${inputValue}>${$(_this).text()}</span>`);
+                } else {
+                  $(_this).replaceWith(`<span id=${e_hover.currentTarget.id} class="custom-class custom-class-replace" title=${inputValue}>${$(_this).text()}</span>`);
+                }
+              } else if (!inputValue && checkTooltip) {
+                checkTooltip = false
+                $(_this).replaceWith($(_this).text());
+              }
+            }
+          });
+        }, function () {
+          $(".custom-class").finish()
+        }
+      );
+      function placeTooltip(x_pos, y_pos) {
+        $("#tooltipHover").css({
+          top: y_pos - 75 + 'px',
+          left: x_pos - 140 + 'px',
+          position: 'absolute'
         });
-      }, function () {
-        $(".custom-class").finish()
       }
-    );
-    function placeTooltip(x_pos, y_pos) {
-      $("#tooltipHover").css({
-        top: y_pos - 80 + 'px',
-        left: x_pos - 120 + 'px',
-        position: 'absolute'
-      });
     }
   }
 
@@ -182,24 +228,30 @@ class Highlighter extends Component {
         </ul>
         <div id="tooltip" className="widget_not_handler hight-light-parent-panel">
           <div className="widget_not_handler hight-light-content-panel">
-            <button type="button" id='addBtn' className="widget_not_handler btn-note-markup-quote"><i className="fas fa-pencil-alt"></i> </button>
+            <div className="widget_not_handler-button">
+              <button onClick={() => this.setState({ key: Math.random() })} type="button" id='addBtn' className="widget_not_handler btn-note-markup-quote"><i className="fas fa-pencil-alt" style={{ fontSize: '18px' }}></i> </button>
+              <button type="button" id='removeBtn' className="widget_not_handler btn-note-markup-quote"><i className="far fa-trash-alt" style={{ fontSize: '18px' }}></i> </button>
+            </div>
             <textarea id='selTxt' name="textSelect" className="widget_not_handler area-markup-quote" placeholder="Ghi chú..."></textarea>
           </div>
         </div>
         <div id="tooltipHover" className="widget_not_handler hight-light-parent-panel">
           <div className="widget_not_handler hight-light-content-panel">
-            <button onClick={() => this.setState({ key: Math.random() })} type="button" id='addBtnHover' className="widget_not_handler btn-note-markup-quote" > <i className="fas fa-pencil-alt"></i> </button>
+            <div className="widget_not_handler-button">
+              <button onClick={() => this.setState({ key: Math.random() })} type="button" id='addBtnHover' className="widget_not_handler btn-note-markup-quote" > <i className="fas fa-pencil-alt" style={{ fontSize: '18px' }}></i> </button>
+              <button type="button" id='removeBtnHover' className="widget_not_handler btn-note-markup-quote"><i className="far fa-trash-alt" style={{ fontSize: '18px' }}></i> </button>
+            </div>
             <textarea onChange={() => this.setState({ key: Math.random() })} id='selTxtHover' name="textSelect" className="widget_not_handler area-markup-quote" placeholder="Ghi chú..."></textarea>
           </div>
         </div>
         <div id="popup" className="dictWordPanel">
           <div className="popupHeader">
             <span style={{ fontWeight: 'bold' }}>{textSelect}</span>
-            <span style={{ position: 'absolute', top: 0, right: 5, cursor: 'pointer' }} onClick={() => document.getElementById('popup').style.display = "none"}><i className="far fa-times-circle"></i></span>
+            <span style={{ position: 'absolute', top: 6, right: 8, cursor: 'pointer' }} onClick={() => document.getElementById('popup').style.display = "none"}><i className="far fa-times-circle"></i></span>
           </div>
           <div dangerouslySetInnerHTML={{ __html: textTranslate }}></div>
         </div>
-        <div id='longtext' style={{ margin: '100px' }} dangerouslySetInnerHTML={{ __html: text }}></div>
+        <div id='longtext' dangerouslySetInnerHTML={{ __html: text }}></div>
       </>
     );
   }
